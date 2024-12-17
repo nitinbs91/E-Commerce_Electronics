95d7908590c6730f67d5451b3a3d4f80b28e2516
Nitin S <nitin.bs.999@gmail.com>
2024-12-18 01:28:08 +0530

    Initial commit - spring initializer mvn built

---
+++ EcomElectronics/src/main/java/ecommerce/electronics/EcomElectronics/EcomElectronicsApplication.java
@@ -0,0 +1,13 @@
+package ecommerce.electronics.EcomElectronics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcomElectronicsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcomElectronicsApplication.class, args);
+	}
+
+}
---
+++ EcomElectronics/src/test/java/ecommerce/electronics/EcomElectronics/EcomElectronicsApplicationTests.java
@@ -0,0 +1,13 @@
+package ecommerce.electronics.EcomElectronics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcomElectronicsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
